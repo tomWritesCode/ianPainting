e6b89df84133fd025a9aa60d270c0d3b337b1eef
Thomas Whitaker <thomaswhitaker@protonmail.com>
2018-03-30 11:32:25 +1100

    Intial Commit - 30/3/18 - 11:32

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.getElementById("openClose").addEventListener("click", function() {
+	// OPEN MENU
+	if (
+		document
+			.getElementById("menu")
+			.classList.contains("menu" && "slideMenuOpen")
+	) {
+		document.getElementById("menu").classList.add("menuMobileOpen");
+		document.getElementById("x").classList.add("xClose");
+		document.getElementById("x").classList.remove("xOpen");
+		document.getElementById("y").classList.add("yClose");
+		document.getElementById("y").classList.remove("yOpen");
+		document.getElementById("menu").classList.remove("slideMenuOpen");
+		console.log("Opened");
+	} else {
+		document.getElementById("menu").classList.remove("menuMobileOpen");
+		document.getElementById("x").classList.remove("xClose");
+		document.getElementById("x").classList.add("xOpen");
+		document.getElementById("y").classList.remove("yClose");
+		document.getElementById("y").classList.add("yOpen");
+		document.getElementById("menu").classList.add("slideMenuOpen");
+		// console.log("Closed");
+	}
+});
