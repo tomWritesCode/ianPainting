9bd75e28b45084adf51565fde2f7199185b284be
Thomas Whitaker <thomaswhitaker@protonmail.com>
2018-07-07 12:00:59 +0100

    7/7/18 - 12:00
    Contact form.
    Changes from Ian to artworks
    fine tuning of CSS and JS

--- js/aboutInfo.js
+++ js/aboutInfo.js
@@ -7,13 +7,10 @@ document.getElementById('tab1').addEventListener('click', () => {
   document.getElementById('info2').classList.remove('show');
   document.getElementById('info3').classList.add('hide');
   document.getElementById('info3').classList.remove('show');
-  document.getElementById('info4').classList.add('hide');
-  document.getElementById('info4').classList.remove('show');
   // Mobile Tab Change
   document.getElementById('tab1').classList.add('tabSelect'); // Mobile Tab Selected
   document.getElementById('tab2').classList.remove('tabSelect');
   document.getElementById('tab3').classList.remove('tabSelect');
-  document.getElementById('tab4').classList.remove('tabSelect');
 });
 document.getElementById('tab2').addEventListener('click', () => {
   document.getElementById('info1').classList.add('hide');
@@ -22,13 +19,10 @@ document.getElementById('tab2').addEventListener('click', () => {
   document.getElementById('info2').classList.remove('hide'); // Show Tab 2
   document.getElementById('info3').classList.add('hide');
   document.getElementById('info3').classList.remove('show');
-  document.getElementById('info4').classList.add('hide');
-  document.getElementById('info4').classList.remove('show');
   // Mobile Tab Change
   document.getElementById('tab1').classList.remove('tabSelect');
   document.getElementById('tab2').classList.add('tabSelect'); // Mobile Tab Selected
   document.getElementById('tab3').classList.remove('tabSelect');
-  document.getElementById('tab4').classList.remove('tabSelect');
 });
 document.getElementById('tab3').addEventListener('click', () => {
   document.getElementById('info1').classList.add('hide');
@@ -37,27 +31,9 @@ document.getElementById('tab3').addEventListener('click', () => {
   document.getElementById('info2').classList.remove('show');
   document.getElementById('info3').classList.add('show');
   document.getElementById('info3').classList.remove('hide'); // Show Tab 3
-  document.getElementById('info4').classList.add('hide');
-  document.getElementById('info4').classList.remove('show');
   // Mobile Tab Change
   document.getElementById('tab1').classList.remove('tabSelect');
   document.getElementById('tab2').classList.remove('tabSelect');
   document.getElementById('tab3').classList.add('tabSelect'); // Mobile Tab Selected
-  document.getElementById('tab4').classList.remove('tabSelect');
-});
-document.getElementById('tab4').addEventListener('click', () => {
-  document.getElementById('info1').classList.add('hide');
-  document.getElementById('info1').classList.remove('show');
-  document.getElementById('info2').classList.add('hide');
-  document.getElementById('info2').classList.remove('show');
-  document.getElementById('info3').classList.add('hide');
-  document.getElementById('info3').classList.remove('show');
-  document.getElementById('info4').classList.add('show'); // Show Tab 4
-  document.getElementById('info4').classList.remove('hide');
-  // Mobile Tab Change
-  document.getElementById('tab1').classList.remove('tabSelect');
-  document.getElementById('tab2').classList.remove('tabSelect');
-  document.getElementById('tab3').classList.remove('tabSelect');
-  document.getElementById('tab4').classList.add('tabSelect'); // Mobile Tab Selected
 });
 
